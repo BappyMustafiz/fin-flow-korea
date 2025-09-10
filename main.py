a7bc688516a8e8247ce4c274fbe7fbb2964c9457
Bappy Mustafiz <mustafizur120133@gmail.com>
2025-09-10 10:18:56 +0600

    end modified for port

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import os
+
 from app import app  # noqa: F401
+from dotenv import load_dotenv
+load_dotenv()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=os.environ.get("PORT", 3000), debug=True)
